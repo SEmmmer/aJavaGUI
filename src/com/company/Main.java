d4ab9c23095758514213669b7bb20c975742e36a
Shirakami Emmmer <yjhdandan@163.com>
2020-03-24 21:11:43 +0800

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,7 +20,7 @@ public class Main {
             frame.pack();
             frame.setVisible(true);
         }
-        
+
         public static void main(String[] args) {
             javax.swing.SwingUtilities.invokeLater(new Runnable() {
                 public void run() {
