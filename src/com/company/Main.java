a90fcbc81be08b87b72a46053c80d51a8ea27845
Shirakami Emmmer <yjhdandan@163.com>
2020-03-24 22:18:16 +0800

    add button

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,32 +3,23 @@ package com.company;
 import javax.swing.*;
 
 public class Main {
-    public static class HelloWorldSwing {
-        private static void createAndShowGUI() {
-            // 确保一个漂亮的外观风格
-            JFrame.setDefaultLookAndFeelDecorated(true);
+    public static void creatGUI() {
+        JFrame frame = new JFrame("Cov-19 Visualized");
+        frame.setSize(700, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JButton button = new JButton("世界概况", null);
 
-            // 创建及设置窗口
-            JFrame frame = new JFrame("covid-19");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-            // 添加 "Hello World" 标签
-            JLabel label = new JLabel("Hello World");
-            frame.getContentPane().add(label);
-
-            // 显示窗口
-            frame.pack();
-            frame.setVisible(true);
-        }
-
-        public static void main(String[] args) {
-            javax.swing.SwingUtilities.invokeLater(new Runnable() {
-                public void run() {
-                    createAndShowGUI();
-                }
-            });
-        }
+        frame.add(button);
+        frame.setVisible(true);
 
 
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                creatGUI();
+            }
+        });
     }
 }
